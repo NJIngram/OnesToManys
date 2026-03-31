4d3a19293cde725458fdc889a7a150731c5cc3d7
nate <Jamalnathan7@gmail.com>
2026-03-31 14:32:05 -0400

    API router

--- backend/json_api.py
+++ backend/json_api.py
@@ -3,6 +3,9 @@ from sqlalchemy.orm import Session
 from backend.main import get_db
 from backend.json_utils import export_all_to_json, import_all_from_json
 import os
-# API router code
+#Create Router instance
+@router.get("/export_json/")
+def export_json():
+    return {"message": "Export endpoint works!"}
 
 
