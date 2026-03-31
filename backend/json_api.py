db971a92446cfd3c8470a4f48e52b0d984b707ac
nate <Jamalnathan7@gmail.com>
2026-03-31 15:09:58 -0400

    Going crazy

--- backend/json_api.py
+++ backend/json_api.py
@@ -4,7 +4,9 @@ from backend.main import get_db
 from backend.json_utils import export_all_to_json, import_all_from_json
 import os
 #Create Router instance
-@router.get("/export_json/")
+router = APIRouter()
+
+@router.get("/export-json/")
 def export_json():
     return {"message": "Export endpoint works!"}
 
